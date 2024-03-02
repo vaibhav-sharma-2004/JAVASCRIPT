44f2b9b87481f53c2bd7ec925bfd3c08bd6dc46d
vaibhav-sharma-2004 <124458256+vaibhav-sharma-2004@users.noreply.github.com>
2024-03-02 18:17:58 +0000

    string (not complete)

---
+++ 1_Basics/5_Strings.js
@@ -0,0 +1,7 @@
+const city="delhi"
+const distance=100
+
+//BEST WAY TO CONCATENATE STRINGS
+console.log(" hello ${city} is ${distance} away from Haryana")
+
+const getName=new String("Vaibhav-Sharma")
